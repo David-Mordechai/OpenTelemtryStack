4c544b2305bb85c46933ff7d344381741aa1810a
David Mordechai <david.mordechai79@gmail.com>
2024-03-25 18:40:39 +0200

    Add project files.

---
+++ WebApi1/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.HttpLogging;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Logging
+    .AddOpenTelemetry(options =>
+    {
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+
+        var resBuilder = ResourceBuilder.CreateDefault();
+        var serviceName = builder.Configuration["ServiceName"]!;
+        resBuilder.AddService(serviceName);
+        options.SetResourceBuilder(resBuilder);
+
+        options.AddOtlpExporter();
+    });
+
+builder.Services.AddHttpLogging(o => o.LoggingFields = HttpLoggingFields.All);
+
+builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(options =>
+{
+    // Filter out instrumentation of the Prometheus scraping endpoint.
+    options.Filter = ctx => ctx.Request.Path != "/metrics";
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(b =>
+    {
+        b.AddService(builder.Configuration["ServiceName"]!);
+    })
+    .WithTracing(b => b
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter())
+    .WithMetrics(b => b
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddProcessInstrumentation()
+        .AddPrometheusExporter());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
+app.UseHttpLogging();
+
+app.MapControllers();
+
+app.Run();
